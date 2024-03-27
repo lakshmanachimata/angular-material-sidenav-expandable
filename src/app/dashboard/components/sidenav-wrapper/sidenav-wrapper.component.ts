6a44e27b1de87de60d3706895ae9ce9f02e0a8f2
Arulprakash <nirmalaarulprakashkaavika@Kaavika.local>
2024-03-27 11:34:51 +0530

    popover submenu html added

--- src/app/dashboard/components/sidenav-wrapper/sidenav-wrapper.component.ts
+++ src/app/dashboard/components/sidenav-wrapper/sidenav-wrapper.component.ts
@@ -3,34 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidenav-wrapper',
   templateUrl: './sidenav-wrapper.component.html',
-  styleUrls: ['./sidenav-wrapper.component.scss']
+  styleUrls: ['./sidenav-wrapper.component.scss'],
 })
 export class SidenavWrapperComponent {
-
   isExpanded: boolean = false;
   isShowing = false;
   showSubmenu = false;
+  popMenu1 = false;
+  popMenu2 = false;
   constructor() {}
 
   toggleSubMenu() {
-    if(this.isExpanded || this.isShowing) {
+    if (this.isExpanded || this.isShowing) {
       this.showSubmenu = !this.showSubmenu;
-      console.log("show SubMenu " + this.showSubmenu);
-    }else{
-      console.log("float SubMenu");
+      console.log('show SubMenu ' + this.showSubmenu);
+    } else {
+      console.log('float SubMenu');
     }
   }
-  showFLMenu(){
-    if(this.isExpanded || this.isShowing) {
-    }else{
-      console.log("float showFLMenu");
+  showFLMenu() {
+    if (this.isExpanded || this.isShowing) {
+    } else {
+      console.log('float showFLMenu');
     }
   }
-  closeFLMenu(){
-    if(this.isExpanded || this.isShowing) {
-    }else{
+  closeFLMenu() {
+    if (this.isExpanded || this.isShowing) {
+    } else {
       setTimeout(() => {
-        console.log("float closeFLMenu");
+        console.log('float closeFLMenu');
       }, 1000);
     }
   }
